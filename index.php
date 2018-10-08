be0d7477c580e1e95d417d5ec3c4bdd6b1621c0d
Werner Roets <cobolt.exe@gmail.com>
2018-10-08 13:57:55 +0200

    cleaned up

--- index.php
+++ index.php
@@ -1,156 +1,191 @@
 <?php
 /**
- * @package aweza-wp
+ * @package aweza-wordpress
  * @version 1.0
  */
+
 /*
-Plugin Name: Aweza for Wordpress
-Plugin URI: https://github.com/fastacademy/aweza-wp
-Description: This plugin allows content to be augmented with the Aweza Popup.
+Plugin Name: Aweza
+Plugin URI: https://github.com/fastacademy/aweza-wordpress
+Description: The official Aweza plugin for WordPress.
 Author: FastAcademy
 Version: 1.0
 Author URI: http://fastacademy.co.za/
 */
-wp_enqueue_style('aweza-popup', plugins_url('aweza-wp') . '/assets/aweza-popup.min.css');
-wp_enqueue_script('aweza-popup', plugins_url('aweza-wp') . '/assets/aweza-popup.min.js');
+define('AWEZA_PLUGIN_NAME', 'aweza-wordpress');
+define('AWEZA_POPUP_VERSION', '3.0.2');
 
-/**
- * @internal never define functions inside callbacks.
- * these functions could be run multiple times; this would result in a fatal error.
- */
+wp_enqueue_style(
+    'aweza-popup-css',
+    plugins_url(AWEZA_PLUGIN_NAME) . '/assets/aweza-popup.min.css',
+    [],
+    AWEZA_POPUP_VERSION
+);
+wp_enqueue_script(
+    'aweza-popup-js',
+    plugins_url(AWEZA_PLUGIN_NAME) . '/assets/aweza-popup.min.js',
+    [],
+    AWEZA_POPUP_VERSION
+);
 
-/**
- * custom option and settings
- */
-function aweza_settings_init() {
-    // register a new setting for "aweza" page
-    register_setting( 'aweza', 'aweza_options' );
+$aweza_api_key = json_encode(get_option('aweza_options')['aweza_key']);
+$aweza_api_secret = json_encode(get_option('aweza_options')['aweza_secret']);
+$aweza_popup_init = <<<SCRIPT
+window.addEventListener('load', function() {
+  window.AwezaPopup({
+    headers: {
+      'AWEZA-KEY': $aweza_api_key,
+      'AWEZA-SECRET': $aweza_api_key
+    },
+    preferLang: 'af'
+  })
+})
+SCRIPT;
 
-    // register a new section in the "aweza" page
-    add_settings_section(
-        'aweza_section_plugin',
-        __( 'Plugin Settings', 'aweza' ),
-        'aweza_section_plugin_cb',
-        'aweza'
-    );
+wp_add_inline_script('aweza-popup-init', $aweza_popup_init);
 
-    // register a new field in the "aweza_section_plugin" section, inside the "aweza" page
-    add_settings_field(
-        'aweza_field_key',
-        __( 'Key', 'aweza' ),
-        'aweza_field_key_cb',
-        'aweza',
-        'aweza_section_plugin',
-        [
-            'label_for' => 'aweza_field_key',
-            'class' => 'aweza_row',
-            'aweza_custom_data' => 'custom',
-        ]
-    );
+add_action('admin_init', ['Aweza_Settings', 'register']);
+add_action('admin_menu', ['Aweza_Menus', 'add_options_page']);
 
-    // register a new field in the "aweza_section_plugin" section, inside the "aweza" page
-    add_settings_field(
-        'aweza_field_secret',
-        __( 'Secret', 'aweza' ),
-        'aweza_field_secret_cb',
-        'aweza',
-        'aweza_section_plugin',
-        [
-            'label_for' => 'aweza_field_secret',
-            'class' => 'aweza_row',
-            'aweza_custom_data' => 'custom',
-        ]
-    );
-}
+class Aweza_Settings
+{
+    private static $page = 'aweza';
+    private static $option_group = 'aweza';
+    private static $option_name = 'aweza_options';
 
-/**
- * register our aweza_settings_init to the admin_init action hook
- */
-add_action( 'admin_init', 'aweza_settings_init' );
+    public static function register()
+    {
+        register_setting(self::$option_group, self::$option_name);
 
+        // Auth Section
+        $section_slug = 'aweza_auth';
+        add_settings_section(
+            $section_slug,
+            'Authentication',
+            [self::class, 'render_auth_section_heading'],
+            self::$page
+        );
 
-// pill field cb
+        self::add_setting_field(
+            'aweza_key',
+            'Key',
+            [self::class, 'render_key_field'],
+            $section_slug
+        );
 
-// field callbacks can accept an $args parameter, which is an array.
-// $args is defined at the add_settings_field() function.
-// wordpress has magic interaction with the following keys: label_for, class.
-// the "label_for" key value is used for the "for" attribute of the <label>.
-// the "class" key value is used for the "class" attribute of the <tr> containing the field.
-// you can add custom key value pairs to be used inside your callbacks.
-function aweza_field_key_cb( $args ) {
-    // get the value of the setting we've registered with register_setting()
-    $options = get_option( 'aweza_options' );
-    // output the field
-    ?>
-    <input id="<?php echo esc_attr( $args['label_for'] ); ?>" name="aweza_options[<?php echo esc_attr( $args['label_for'] ); ?>]">
+        self::add_setting_field(
+            'aweza_secret',
+            'Secret',
+            [self::class, 'render_secret_field'],
+            $section_slug
+        );
 
-    <?php
-}
+        // Popup section
+        $section_slug = 'aweza_popup';
+        add_settings_section($section_slug,
+            'Popup',
+            [self::class, 'render_popup_section_heading'],
+            self::$page
+        );
 
-function aweza_field_secret_cb( $args ) {
-    // get the value of the setting we've registered with register_setting()
-    $options = get_option( 'aweza_options' );
-    // output the field
-    ?>
-    <input type="password" id="<?php echo esc_attr( $args['label_for'] ); ?>" name="aweza_options[<?php echo esc_attr( $args['label_for'] ); ?>]">
+        self::add_setting_field(
+            'aweza_prefer_lang',
+            'Prefer Lang',
+            [self::class, 'render_prefer_lang_field'],
+            $section_slug
+        );
 
-    <?php
-}
-
-/**
- * top level menu
- */
-function aweza_options_page() {
-    // add top level menu page
-    add_menu_page(
-        'Aweza',
-        'Aweza Options',
-        'manage_options',
-        'aweza',
-        'aweza_options_page_html'
-    );
-}
-
-/**
- * register our aweza_options_page to the admin_menu action hook
- */
-add_action( 'admin_menu', 'aweza_options_page' );
-
-/**
- * top level menu:
- * callback functions
- */
-function aweza_options_page_html() {
-    // check user capabilities
-    if ( ! current_user_can( 'manage_options' ) ) {
-        return;
     }
 
-    // add error/update messages
-
-    // check if the user have submitted the settings
-    // wordpress will add the "settings-updated" $_GET parameter to the url
-    if ( isset( $_GET['settings-updated'] ) ) {
-        // add settings saved message with the class of "updated"
-        add_settings_error( 'aweza_messages', 'aweza_message', __( 'Settings Saved', 'aweza' ), 'updated' );
-    }
-
-    // show error/update messages
-    settings_errors( 'aweza_messages' );
-    ?>
-    <div class="wrap">
-        <h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
+    public static function render_options_page()
+    {
+        ?>
+      <div class="wrap">
+        <h1><?= esc_html(get_admin_page_title()); ?></h1>
         <form action="options.php" method="post">
             <?php
-            // output security fields for the registered setting "aweza"
-            settings_fields( 'aweza' );
-            // output setting sections and their fields
-            // (sections are registered for "aweza", each field is registered to a specific section)
-            do_settings_sections( 'aweza' );
-            // output save settings button
-            submit_button( 'Save Settings' );
+            settings_fields(self::$option_group);
+            do_settings_sections(self::$page);
+            submit_button('Save Settings');
             ?>
         </form>
-    </div>
-    <?php
+      </div>
+        <?php
+    }
+
+    public static function render_popup_section_heading()
+    {
+        echo 'Customise the Popup\'s behaviour.';
+    }
+
+    public static function render_auth_section_heading()
+    {
+        echo 'Enter your Aweza API credentials here.';
+    }
+
+    public static function render_key_field($args)
+    {
+        $key = get_option(self::$option_name)['aweza_key'];
+        ?>
+      <input id="<?= esc_attr($args['label_for']); ?>"
+             name="aweza_options[<?= esc_attr($args['label_for']); ?>]"
+             value="<?= $key ?>">
+        <?php
+    }
+
+    public static function render_secret_field($args)
+    {
+        $secret = get_option(self::$option_name)['aweza_secret'];
+        ?>
+      <input id="<?= esc_attr($args['label_for']); ?>"
+             name="aweza_options[<?= esc_attr($args['label_for']); ?>]"
+             value="<?= $secret ?>">
+        <?php
+    }
+
+    public static function render_prefer_lang_field($args)
+    {
+        $prefer_lang = get_option(self::$option_name)['aweza_prefer_lang'];
+        ?>
+      <input id="<?= esc_attr($args['label_for']); ?>"
+             name="aweza_options[<?= esc_attr($args['label_for']); ?>]"
+             value="<?= $prefer_lang ?>">
+        <?php
+    }
+
+    private static function add_setting_field($slug, $title, $renderer, $section_slug)
+    {
+        add_settings_field(
+            $slug,
+            $title,
+            $renderer,
+            self::$page,
+            $section_slug,
+            ['label_for' => $slug]
+        );
+    }
 }
+
+
+class Aweza_Menus
+{
+    private static $page_title = 'Aweza Settings';
+    private static $menu_title = 'Aweza';
+    private static $capability = 'manage_options';
+    private static $menu_slug = 'aweza';
+    private static $render_cb = ['Aweza_Settings', 'render_options_page'];
+
+    public static function add_options_page()
+    {
+        add_options_page(
+            self::$page_title,
+            self::$menu_title,
+            self::$capability,
+            self::$menu_slug,
+            self::$render_cb
+        );
+    }
+
+
+}
+
